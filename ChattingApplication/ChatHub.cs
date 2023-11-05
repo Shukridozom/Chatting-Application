9e44f4606d3d25c86437233904321964445a5a4b
Shukridozom <Shukridozom@hotmail.com>
2023-11-05 19:38:54 +0200

    Storing the received messages into the database

--- ChattingApplication/ChatHub.cs
+++ ChattingApplication/ChatHub.cs
@@ -1,4 +1,5 @@
-﻿using ChattingApplication.Core.Domains;
+﻿using ChattingApplication.Core;
+using ChattingApplication.Core.Domains;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
@@ -7,13 +8,34 @@ namespace ChattingApplication
     [Authorize(Roles = RoleName.Verified)]
     public class ChatHub : Hub
     {
-        public ChatHub()
-        {
+        private readonly IUnitOfWork _unitOfWork;
 
+        public ChatHub(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
         }
         public async Task SendMessage(string user, string message)
         {
             await Clients.User(user).SendAsync("ReceiveMessage", Context.UserIdentifier, message);
+            SaveMessage(Convert.ToInt32(Context.UserIdentifier), user, message);
+        }
+
+        private void SaveMessage(int senderId, string receiverUsername, string body)
+        {
+            var receiver = _unitOfWork.Users.SingleOrDefault(u => u.Username == receiverUsername);
+            if (receiver == null)
+                return;
+
+            var message = new Message()
+            {
+                SenderId = senderId,
+                ReceiverId = receiver.Id,
+                Date = DateTime.Now,
+                Body = body
+            };
+
+            _unitOfWork.Messages.Add(message);
+            _unitOfWork.Complete();
 
         }
     }
