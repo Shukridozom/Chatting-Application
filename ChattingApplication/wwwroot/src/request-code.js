d0fb03597536df0cb25843f94f45dda7529c9f77
Shukridozom <Shukridozom@hotmail.com>
2023-12-01 02:09:35 +0300

    using validation messages in request-code page

--- ChattingApplication/wwwroot/src/request-code.js
+++ ChattingApplication/wwwroot/src/request-code.js
@@ -11,6 +11,47 @@ $(document).ready(function() {
             401: function(){
                 $("#nav-item-logout").remove();
                 $("body").css("visibility", "visible");
+
+                $('#request-code-form').on('submit', function(event) {
+                    var settings = {
+                        "url":  domain + `/api/requestCode?email=${$("#email").val()}`,
+                        "method": "POST",
+                        "timeout": 0,
+                        "headers": {
+                          "Content-Type": "application/json"
+                        },
+                        "complete": function(response) {
+                            switch (response.status) {
+                                case 200:
+                                    window.location.replace(domain + "/reset-password.html");
+                                    break;
+                                default:
+                                    $(".validation-message").remove();
+                                    var errors = JSON.parse(response.responseText);
+                                    if('errors' in errors) {
+                                        for(let key in errors.errors) {
+                                            if(key === 'alert') {
+                                                $("#request-code-alert").css("bottom", "0rem");
+                                                setTimeout(function(){
+                                                    $("#request-code-alert").css("bottom", "-10rem");
+                                                },2000);
+                                                
+                                                continue;
+                                            }
+                                            $(`#${key}`).after(`<small class="form-text text-muted validation-message" style="color: red !important;">${errors.errors[key][0]}</small>`);
+                                        }
+                                        $("#btn-request-code").prop("disabled", false);
+                                    }
+                                    break;
+                                }
+                            }
+                        };
+                        
+                        $.ajax(settings);
+                        $("#btn-request-code").prop("disabled", true);
+
+                    event.preventDefault();
+                  });
             },
             200: function(res){
                 window.location.replace(domain + "/index.html");
